c1cf76ed7b58f1b58ad42b4bea26a44607fd7460
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-10-20 13:58:12 -0700

    init commit

---
+++ includes/include.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/../lib/Fizzik/fizzik-autoloader.php';
+require_once __DIR__ . '/Credentials.php';
+require_once __DIR__ . '/HotstatusPipeline.php';
---
+++ lib/README.md
@@ -0,0 +1,3 @@
+<b>CGI Libraries (cgi/lib)</b>
+- [AWS Php SDK](https://aws.amazon.com/sdk-for-php/)
+- [Fizzik](https://github.com/maximtiourin/Fizzik)
