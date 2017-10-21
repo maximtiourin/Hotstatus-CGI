c86ed270d03bb93a142d27c5092ec1299858c6df
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-10-21 15:08:45 -0700

    Updated find process to try a few more times to get a result with replays in it before deciding the API has no more replays to offer at this time, due to a strange bug where the result will have an empty replays array from time to time even though the page should have replays

--- replayprocess_find.php
+++ replayprocess_find.php
@@ -21,12 +21,15 @@ $db->connect($creds[Credentials::KEY_DB_HOSTNAME], $creds[Credentials::KEY_DB_US
 $db->setEncoding(HotstatusPipeline::DATABASE_CHARSET);
 
 //Constants and qol
-const OUT_OF_REPLAYS_SLEEP_DURATION = 3600; //seconds
+const OUT_OF_REPLAYS_COUNT_LIMIT = 5; //5 occurence limit
+const OUT_OF_REPLAYS_COUNT_DURATION = 60; //seconds
+const OUT_OF_REPLAYS_SLEEP_DURATION = 1800; //seconds
 const UNKNOWN_ERROR_CODE = 300; //seconds
 const TOO_MANY_REQUEST_SLEEP_DURATION = 60; //seconds
 const SLEEP_DURATION = 5; //seconds
 const MINI_SLEEP_DURATION = 1; //seconds
 const E = PHP_EOL;
+$out_of_replays_count = 0; //Count how many times we ran out of replays to process, once reaching limit it means the api isn't bugging out and there actually is no more replays.
 $sleep = new SleepHandler();
 
 //Prepare statements
@@ -112,9 +115,20 @@ while (true) {
             }
         }
         else {
-            //No more replay pages to process! Long sleep
-            echo timestamp() . 'Out of replays to process! Waiting for new hotsapi replay at page index #' . $pageindex . '...'.E;
-            $sleep->add(OUT_OF_REPLAYS_SLEEP_DURATION);
+            $out_of_replays_count++;
+
+            if ($out_of_replays_count >= OUT_OF_REPLAYS_COUNT_LIMIT) {
+               //No more replay pages to process! Long sleep
+               $out_of_replays_count = 0;
+
+               echo timestamp() . 'Out of replays to process! Waiting for new hotsapi replay at page index #' . $pageindex . '...'.E;
+               $sleep->add(OUT_OF_REPLAYS_SLEEP_DURATION);
+            }
+            else {
+               //Potentially no more replay pages to process, try a few more times after a minute to make sure it's not just the API bugging out.
+               echo timestamp() . 'Received empty replays result...'.E;
+               $sleep->add(OUT_OF_REPLAYS_COUNT_DURATION);
+            }
         }
     }
     else if ($api['code'] == Hotsapi::HTTP_RATELIMITED) {
