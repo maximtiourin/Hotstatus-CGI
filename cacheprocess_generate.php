d029ea4cfea215367cda31dd6428498e519ff452
Maxim Tiourin <mixmaxtwo@gmail.com>
2017-12-14 18:48:32 -0800

    Implemented filter fragment generation for generate cache process

--- cacheprocess_generate.php
+++ cacheprocess_generate.php
@@ -18,6 +18,52 @@ $db->setEncoding(HotstatusPipeline::DATABASE_CHARSET);
 //Constants and qol
 const E = PHP_EOL;
 
+//Functions
+
+/*
+ * Given a filter key (EX: 'gameType'), and an active map, where active map =
+ * [
+ *      "selection key" => ["selected" => TRUE||FALSE],
+ *      ...,
+ *      ...,
+ * ]
+ *
+ * returns a filter fragment object of type
+ * [
+ *  "key" => '$key',
+ *  "value" => '$value' || '$value1,$value2,$value3,...,$valueN'
+ * ]
+ */
+function generateFilterFragment($key, $activemap) {
+    $activevals = [];
+
+    foreach ($activemap as $akey => $aobj) {
+        if ($aobj['selected'] === TRUE) {
+            $activevals[] = $akey;
+        }
+    }
+
+    $val = "";
+
+    if (count($activevals) > 0) {
+        $val = join(",", $activevals);
+    }
+
+    return [
+        "key" => $key,
+        "value" => $val,
+    ];
+}
+
+//Test definition
+$test = [
+    "generateFilterFragment" => function() {
+        $test = generateFilterFragment("map", HotstatusPipeline::$filter[HotstatusPipeline::FILTER_KEY_MAP]);
+        echo $test['key'] . "=" . $test['value'] .E;
+    }
+];
+
 //Prepare statements
+
 
 ?>
