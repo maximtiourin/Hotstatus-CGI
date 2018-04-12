dec4f3848b9262a7b835f4f3a1ec041dde2fb2f5
Maxim Tiourin <mixmaxtwo@gmail.com>
2018-04-12 05:43:03 -0700

    Added running data purge process

--- utilityprocess_database_purge_olddata.php
+++ utilityprocess_database_purge_olddata.php
@@ -8,6 +8,7 @@ namespace Fizzik;
 require_once 'includes/include.php';
 
 use Fizzik\Database\MySqlDatabase;
+use Fizzik\Utility\SleepHandler;
 
 set_time_limit(0);
 date_default_timezone_set(HotstatusPipeline::REPLAY_TIMEZONE);
@@ -19,26 +20,39 @@ $db->setEncoding(HotstatusPipeline::DATABASE_CHARSET);
 
 //Constants and qol
 const E = PHP_EOL;
+const SLEEP_DURATION = 1;
+$sleep = new SleepHandler();
+
+//Helper Functions
+function log($str) {
+    $datetime = new \DateTime("now");
+    $datestr = $datetime->format(HotstatusPipeline::FORMAT_DATETIME);
+    echo "[$datestr] $str".E;
+}
 
 //Mininum Date Inclusive for replays to process
-$replaymindate = HotstatusPipeline::$SEASONS[HotstatusPipeline::SEASON_UNKNOWN]["end"];
+//$replaymindate = HotstatusPipeline::$SEASONS[HotstatusPipeline::SEASON_UNKNOWN]["end"];
+
+$replaymindate = "2018-01-01 00:00:00";
 
 //Purge old data
-$db->transaction_begin();
-try {
-    $db->query("DELETE FROM `heroes_bans_recent_granular` WHERE `date_end` < \"$replaymindate\"");
-    $db->query("DELETE FROM `heroes_matches_recent_granular` WHERE `date_end` < \"$replaymindate\"");
-    $db->query("DELETE FROM `matches` WHERE `date` < \"$replaymindate\"");
-    $db->query("DELETE FROM `players_matches` WHERE `date` < \"$replaymindate\"");
-    $db->query("DELETE FROM `players_matches_recent_granular` WHERE `date_end` < \"$replaymindate\"");
-    $db->query("DELETE FROM `players_mmr` WHERE `season` = \"2017 Season 2\"");
+//$db->transaction_begin();
 
-    $db->transaction_commit();
-}
-catch (\Exception $e) {
-    $db->transaction_rollback();
-}
+$db->prepare("delete_heroes_bans", "DELETE FROM `heroes_bans_recent_granular` WHERE `date_end` < \"$replaymindate\" ORDER BY `date_end` ASC LIMIT 1000");
+$db->prepare("delete_heroes_matches", "DELETE FROM `heroes_matches_recent_granular` WHERE `date_end` < \"$replaymindate\" ORDER BY `date_end` ASC LIMIT 1000");
 
-$db->close();
+
+while (TRUE) {
+    $db->execute("delete_heroes_bans");
+    $db->execute("delete_heroes_matches");
+    log("Purged Heroes data older than [$replaymindate].");
+    //$db->query("DELETE FROM `matches` WHERE `date` < \"$replaymindate\"");
+    //$db->query("DELETE FROM `players_matches` WHERE `date` < \"$replaymindate\"");
+    //$db->query("DELETE FROM `players_matches_recent_granular` WHERE `date_end` < \"$replaymindate\"");
+    //$db->query("DELETE FROM `players_mmr` WHERE `season` = \"2017 Season 2\"");
+    $sleep->add(SLEEP_DURATION);
+
+    $sleep->execute();
+}
 
 ?>
