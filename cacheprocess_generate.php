9d65f8dbc3a9184ccdd02d99274845f47e5077e7
Maxim Tiourin <mixmaxtwo@gmail.com>
2018-05-10 09:33:09 -0700

    date typo

--- cacheprocess_generate.php
+++ cacheprocess_generate.php
@@ -25,7 +25,7 @@ $target_date_range = "Last 7 Days";
 
 //$target_patch_range = "2.32.2 (Balance)";
 $target_patch = HotstatusPipeline::$PATCHES[HotstatusPipeline::PATCH_CURRENT];
-$target_date_range = $target_patch['version']." (".$target_patch['type'].")";
+$target_patch_range = $target_patch['version']." (".$target_patch['type'].")";
 
 //Prepare statements
 $db->prepare("GetPipelineConfig",
